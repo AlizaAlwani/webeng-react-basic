dab60d8a07ac42bab116441265a27131112b3744
AlizaAlwani <aliza.alwani@hotmail.com>
2024-05-17 12:02:48 +0500

    Final changes

--- components/componentC.tsx
+++ components/componentC.tsx
@@ -10,9 +10,9 @@ const ComponentC = ({counter, setcounter}: ComponentType) => {
   return (
     <div className='border border-black bg-green-200 h-[75%] w-[75%] rounded-lg items-center justify-center flex flex-col gap-4'>
         ComponentC
-        <div className=''>
+        <div className='flex gap-2 items-center justify-center'>
           <Button variant='destructive' onClick={ () => setcounter (prev => prev-1)}> - </Button>
-          {counter}
+          <div>{counter}</div>
           <Button onClick={ () => setcounter (prev => prev+1)}> + </Button>
         </div>
      
